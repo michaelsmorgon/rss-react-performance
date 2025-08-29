721bf8f7e9d72a025001a615034bdf3a5e6361d0
michaelsmorgon <smorgon@int.pl>
2025-08-30 00:30:29 +0200

    feat: change eslint config

--- rs-react-performance/eslint.config.js
+++ rs-react-performance/eslint.config.js
@@ -2,7 +2,10 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from "eslint-plugin-react";
 import tseslint from 'typescript-eslint'
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import reactCompiler from "eslint-plugin-react-compiler";
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
@@ -11,13 +14,35 @@ export default tseslint.config([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.strict,
+      eslintPluginPrettier,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "react-compiler": reactCompiler,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react-compiler/react-compiler": "error",
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
 ])
